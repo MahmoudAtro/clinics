5e8a4221edb63c8f5bf45a957986312e9fc36589
MahmoudAtro <mahmoudyatro@gmail.com>
2024-10-25 18:37:10 +0300

    added firebase option file added firebase to project added firebase_core package

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:clinics/core/di/dependency_injection.dart';
 import 'package:clinics/core/routing/app_route.dart';
 import 'package:clinics/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  await ScreenUtil.ensureScreenSize();
   setupGetIt();
   runApp(
     DocApp(
